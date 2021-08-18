880b8078782437a49eb18cf7290b2015f37f435e
jmm9683 <jakemorrissey@utexas.edu>
2021-08-18 00:15:18 -0500

    image formatting v1

--- components/albumsPage.js
+++ components/albumsPage.js
@@ -1,59 +1,41 @@
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  Image,
+} from "react-native";
 import React, { Component } from "react";
+// import Image from "react-native-scalable-image";
+
+const { width, height } = Dimensions.get("window");
 
 class AlbumsPage extends Component {
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.scrollView}>
-          <Text style={styles.text}>
+          <Image
+            source={require("./../assets/IMG_2156.jpeg")}
+            style={styles.albumCoverImage}
+          />
+          <Image
+            source={require("./../assets/IMG_0735.jpeg")}
+            style={styles.albumCoverImage}
+          />
+          <Image
+            source={require("./../assets/IMG_0657.jpeg")}
+            style={styles.albumCoverImage}
+          />
+          {/* <Text style={styles.text}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur
-          </Text>
-          <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur
-          </Text>
-          <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur
-          </Text>
-          <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur
-          </Text>
-          <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur
-          </Text>
-          <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur
-          </Text>
+          </Text> */}
         </ScrollView>
       </View>
     );
@@ -67,7 +49,6 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     flex: 1,
-    marginHorizontal: 20,
     marginTop: 35,
   },
   text: {
@@ -77,6 +58,12 @@ const styles = StyleSheet.create({
     lineHeight: 42,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  albumCoverImage: {
+    width: Dimensions.get("window").width * 0.75,
+    height: Dimensions.get("window").width * 0.75,
+    resizeMode: "contain",
+    alignSelf: "center",
   },
 });
 
