a1697b2c556e31fbf4d20c8741c326159421654a
jmm9683 <jakemorrissey@utexas.edu>
2021-08-29 21:47:35 -0700

    pinch to zoom

--- components/albumsPage.js
+++ components/albumsPage.js
@@ -5,14 +5,18 @@ import {
   ScrollView,
   Dimensions,
   Image,
-  ImageBackground,
-  Platform,
+  TouchableWithoutFeedback,
 } from "react-native";
 import Carousel, { Pagination } from "react-native-snap-carousel";
-import React, { Component, useRef, useState, useEffect } from "react";
+import ImageZoom, { reset } from "react-native-image-pan-zoom";
+import React, { Component } from "react";
 // import Image from "react-native-scalable-image";
 
 const { width, height } = Dimensions.get("window");
+
+let scrollEnabler = true;
+let scrollFactor = 0.85;
+let emptyScrollFactor = 0.5 * (1 - scrollFactor);
 
 const ENTRIES1 = [
   {
@@ -45,16 +49,37 @@ const ENTRIES1 = [
 
 class AlbumsPage extends Component {
   _renderItem({ item, index }, parallaxProps) {
+    let _onPress = () => () => {
+      // scrollEnabler = !scrollEnabler;
+    };
     return (
       <View style={styles.item}>
-        <Image
-          source={ENTRIES1[index].uri}
-          style={styles.image}
-          blurRadius={0}
-        />
-        <Text style={styles.title} numberOfLines={2}>
-          {ENTRIES1[index].title}
-        </Text>
+        <TouchableWithoutFeedback onPress={_onPress()}>
+          <ImageZoom
+            cropWidth={width}
+            cropHeight={height * 0.85}
+            imageWidth={width}
+            imageHeight={height * 0.85}
+            panToMove={false}
+            onDoubleClick={() => {
+              ImageZoom.reset;
+            }}
+            onMove={() => {
+              ImageZoom.reset;
+            }}
+          >
+            <Image
+              source={ENTRIES1[index].uri}
+              style={styles.image}
+              blurRadius={0}
+            />
+          </ImageZoom>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={_onPress()}>
+          <Text style={styles.title} numberOfLines={2}>
+            {ENTRIES1[index].title}
+          </Text>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
@@ -62,34 +87,15 @@ class AlbumsPage extends Component {
     return (
       <View style={styles.container}>
         <ScrollView
-          style={styles.scrollView}
+          style={styles.container}
           decelerationRate={0}
-          snapToInterval={height * 0.5}
+          snapToInterval={height * scrollFactor}
+          snapToAlignment="start"
+          scrollEnabled={scrollEnabler}
+          stickyHeaderIndices={[0]}
         >
-          <View style={styles.emptySpace}></View>
-          <View style={styles.albumCoverContainer}>
-            <Carousel
-              layout={"stack"}
-              renderItem={this._renderItem}
-              sliderWidth={width}
-              itemWidth={width}
-              data={ENTRIES1}
-              inactiveSlideOpacity={0.7}
-              inactiveSlideScale={0.9}
-              inactiveSlideShift={0}
-            />
-          </View>
-          <View style={styles.albumCoverContainer}>
-            <Carousel
-              layout={"stack"}
-              renderItem={this._renderItem}
-              sliderWidth={width}
-              itemWidth={width}
-              data={ENTRIES1}
-              inactiveSlideOpacity={0.7}
-              inactiveSlideScale={0.9}
-              inactiveSlideShift={0}
-            />
+          <View style={[styles.emptySpace, styles.topNav]}>
+            <Text style={[styles.titleTopNav, styles.topNav]}>My Albums</Text>
           </View>
           <View style={styles.albumCoverContainer}>
             <Carousel
@@ -128,22 +134,6 @@ class AlbumsPage extends Component {
             />
           </View>
           <View style={styles.emptySpace}></View>
-          {/* <View style={styles.albumCoverContainer}>
-            <Image
-              source={require("./../assets/IMG_0735.jpeg")}
-              style={styles.albumCoverImage}
-              blurRadius={0}
-            />
-            <Text style={styles.title}>Stevens Peak</Text>
-          </View>
-          <View style={styles.albumCoverContainer}>
-            <Image
-              source={require("./../assets/IMG_0657.jpeg")}
-              style={styles.albumCoverImage}
-              blurRadius={0}
-            />
-            <Text style={styles.title}>Lake Pend Oreille</Text>
-          </View> */}
         </ScrollView>
       </View>
     );
@@ -175,30 +165,35 @@ const styles = StyleSheet.create({
     // fontWeight: "bold",
     textAlign: "center",
   },
-  albumCoverImage: {
-    width: width * 1,
-    height: height * 1,
-    resizeMode: "contain",
-    alignSelf: "center",
-  },
   albumCoverContainer: {
     flex: 1,
   },
   item: {
     width: width,
-    height: height * 0.5,
+    height: height * scrollFactor,
     // marginTop: "20%",
   },
   emptySpace: {
     width: width,
-    height: height * 0.25,
+    height: height * emptyScrollFactor,
   },
   image: {
-    height: -1000,
-    width: -1000,
+    height: -1,
+    width: -1,
     ...StyleSheet.absoluteFillObject,
     resizeMode: "contain",
     alignSelf: "center",
+  },
+  topNav: {
+    backgroundColor: "black",
+  },
+  titleTopNav: {
+    position: "absolute",
+    top: "50%",
+    width: "100%",
+    color: "white",
+    fontSize: 25,
+    textAlign: "center",
   },
 });
 
