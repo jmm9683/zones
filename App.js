d19e02b1e1df1de13842231e8891c5ee204e4679
jmm9683 <jakemorrissey@utexas.edu>
2021-09-02 17:20:47 -0700

    back button fix

--- App.js
+++ App.js
@@ -5,19 +5,43 @@ import AlbumsPage from "./components/albumsPage";
 import Album from "./components/album";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HeaderBackButton } from "react-navigation-stack";
 // import { createSharedElementStackNavigator } from "react-navigation-shared-element";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={LandingPage} />
-        <Stack.Screen name="Album" component={Album} />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={LandingPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Album"
+          component={Album}
+          options={{
+            title: "",
+            headerTransparent: true,
+            headerTitleStyle: {
+              color: "white",
+            },
+            headerTintColor: "white",
+            headerStyle: {
+              position: "absolute",
+              backgroundColor: "transparent",
+              color: "white",
+              zIndex: 100,
+              top: 0,
+              left: 0,
+              right: 0,
+              elevation: 0,
+              shadowOpacity: 0,
+              borderBottomWidth: 0,
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
