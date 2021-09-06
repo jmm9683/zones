03f341b15b252cca3d507401f5598bb979c485e6
Jake Morrissey <jakemorrissey@utexas.edu>
2021-09-06 15:25:55 -0500

    collage effect attempt 1

--- components/albumsPage.js
+++ components/albumsPage.js
@@ -18,8 +18,9 @@ const { width, height } = Dimensions.get("window");
 
 let scalarWidth = width * 0.5;
 let scalarHeight = height * 0.25;
-
-const ENTRIES1 = [
+const IMAGE_SIZE = 30;
+const SPACING = 10;
+const ENTRIES = [
   {
     title: "Nevada",
     subtitle: "Lorem ipsum dolor sit amet et nuncat mergitur",
@@ -31,9 +32,18 @@ const ENTRIES1 = [
     title: "",
     // subtitle: "Lorem ipsum dolor sit amet",
     illustration:
-      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_2156.jpeg",
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0735.jpeg",
+    uri: require("./../assets/IMG_0735.jpeg"),
+    id: "3",
+  },
+];
+const ENTRIES1 = [
+  {
+    title: "Nevada",
+    subtitle: "Lorem ipsum dolor sit amet et nuncat mergitur",
+    illustration: "",
     uri: require("./../assets/IMG_2156.jpeg"),
-    id: "2",
+    id: "1",
   },
   {
     title: "",
@@ -51,102 +61,102 @@ const ENTRIES1 = [
     uri: require("./../assets/IMG_0657.jpeg"),
     id: "4",
   },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "41",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "411",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "41111",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "411111",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "4111111",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "41111111",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "411111111",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "4111111111",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "41111111111",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "411111111111",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "4111111111111",
-  // },
-  // {
-  //   title: "",
-  //   // subtitle: "Lorem ipsum dolor sit amet",
-  //   illustration:
-  //     "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
-  //   uri: require("./../assets/IMG_0657.jpeg"),
-  //   id: "41111111111111",
-  // },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "41",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "411",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "41111",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "411111",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "4111111",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "41111111",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "411111111",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "4111111111",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "41111111111",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "411111111111",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "4111111111111",
+  },
+  {
+    title: "",
+    // subtitle: "Lorem ipsum dolor sit amet",
+    illustration:
+      "file:///Users/jakemorrissey/Documents/WebDevTraining/zones/assets/IMG_0657.jpeg",
+    uri: require("./../assets/IMG_0657.jpeg"),
+    id: "41111111111111",
+  },
 ];
 
 const albums = [
@@ -157,7 +167,7 @@ const albums = [
   },
   {
     title: "Utah",
-    images: ENTRIES1,
+    images: ENTRIES,
     id: "2",
   },
   {
@@ -167,7 +177,7 @@ const albums = [
   },
   {
     title: "Idaho",
-    images: ENTRIES1,
+    images: ENTRIES,
     id: "4",
   },
 ];
@@ -180,7 +190,38 @@ const AlbumItem = ({ item }) => {
     >
       <View style={styles.albumCoverContainer}>
         <View style={styles.item}>
-          <Image source={item.images[0].uri} style={styles.image} />
+          <FlatList
+            data={item.images.slice(0,9)}
+            keyExtractor={(item) => item.id}
+            style={styles.item}
+            numColumns={3}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: SPACING, justifyContent: "center" }}
+            renderItem={({ item }) => {
+              return (
+                  <Image
+                    source={item.uri}
+                    style={{
+                      height: scalarWidth/3,
+                      width: scalarWidth/3,
+                      flex: 1,
+                      // aspectRatio:1,
+                      resizeMode:'cover',
+                      shadowColor: "#000",
+                      shadowOffset: {
+                        width: 0,
+                        height: 3,
+                      },
+                      shadowOpacity: 0.29,
+                      shadowRadius: 4.65,
+                      borderWidth: 2,
+                      borderColor: "black",
+                      borderRadius: 10
+                    }}
+              />
+          );
+        }}
+      />
         </View>
       </View>
       <Text style={styles.title}>{item.title}</Text>
@@ -190,7 +231,7 @@ const AlbumItem = ({ item }) => {
 
 class AlbumsPage extends Component {
   renderItem({ item }) {
-    return <AlbumItem item={item} />;
+    return <AlbumItem item={item}/>;
   }
 
   render() {
@@ -200,7 +241,7 @@ class AlbumsPage extends Component {
           data={albums}
           renderItem={this.renderItem}
           keyExtractor={(item) => item.id}
-          numColumns={2}
+          numColumns={1}
           initialNumToRender={6}
         />
       </SafeAreaView>
@@ -222,16 +263,18 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   albumCoverContainer: {
-    flex: 1,
-    padding: 5,
+    flex: 3,
+    padding: 50
   },
   item: {
     width: scalarWidth,
     height: scalarHeight,
+    flex: 3,
+    alignSelf: "center"
   },
   image: {
-    height: -1,
-    width: -1,
+    height: "100%",
+    width: "100%",
     ...StyleSheet.absoluteFillObject,
     resizeMode: "contain",
     alignSelf: "center",
@@ -242,6 +285,9 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.29,
     shadowRadius: 4.65,
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 25
   },
 });
 
